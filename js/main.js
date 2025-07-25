bdcb382b22045bcb0f1e0c303f6173f21af7b028
alaminkhanstd123 <alaminkhanstd123@gmail.com>
2025-07-25 18:32:39 +0600

    counter up added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+const counterUp = window.counterUp.default
+
+const callback = entries => {
+    entries.forEach(entry => {
+        const el = entry.target
+        if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+            counterUp(el, {
+                duration: 2000,
+                delay: 16,
+            })
+            el.classList.add('is-visible')
+        }
+    })
+}
+
+const IO = new IntersectionObserver(callback, { threshold: 1 })
+
+// Select all elements with the class 'counter'
+const counters = document.querySelectorAll('.counter')
+
+// Observe each one
+counters.forEach(counter => IO.observe(counter))
 
 // added country list to the form
 const countryUrl = "https://restcountries.com/v3.1/independent?status=true";
@@ -127,3 +149,4 @@ document.addEventListener('DOMContentLoaded', ()=>{
 })
 
 // send email using email js
+
